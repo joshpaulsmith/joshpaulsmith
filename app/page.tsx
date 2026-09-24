6c03114ef9585e4e0b0db8760c4fbd7229566d30
jps <91107393+joshpaulsmith@users.noreply.github.com>
2026-09-24 17:20:50 -0400

    Update Site source

--- app/page.tsx
+++ app/page.tsx
@@ -70,12 +70,11 @@ export default function Home() {
             <p>My background is primarily in business and operations. Open communication, responsible decision making, managing priorities and finding practical solutions have served me well throughout my career, and I would bring that same approach to council.</p>
             <p>For me, running for council is about giving back to a community that has given my family so much while making thoughtful, responsible decisions for its future.</p>
             <p>I am proud of my family’s deep roots here, proud to be raising my children here, and ready to do my part for the future of the Township of St. Joseph. I hope I can count on your support in the upcoming election.</p>
+            <p className="signature">Joshua P. Smith</p>
           </div>
         </div>
 
         <div className="letter-closing">
-          <p className="signature">Joshua Smith</p>
-
           <figure className="letter-family-photo">
             <Image
               src="/family.jpeg"
